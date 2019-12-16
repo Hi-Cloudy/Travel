c17e9046323841724cf05cc6c50d9eb3c9c85382
Cloud <1071522794@qq.com>
2019-12-16 18:40:38 +0800

    finish detail module

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
    <!-- <img src="./assets/logo.png"> -->
-   <keep-alive><!-- 加载后从内存将在 -->
+   <keep-alive exclude="Detail"><!-- 加载后从内存获取 -->
      <router-view/>
    </keep-alive>
   </div>
